e250b10b1a403de69866d949249ad19878f517c7
Shreya Sthapit <healthmandala@gmail.com>
2026-04-13 07:09:05 +0545

    feat: add minutesToTime formatter to timeUtils

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -10,3 +10,12 @@ const calculateTokens = (startTime, endTime, lunchStart, lunchEnd, slotMinutes =
 };
 
 module.exports = { timeToMinutes, calculateTokens };
+
+// Format minutes back to HH:MM
+const minutesToTime = (minutes) => {
+  const h = Math.floor(minutes / 60).toString().padStart(2, '0');
+  const m = (minutes % 60).toString().padStart(2, '0');
+  return `${h}:${m}`;
+};
+
+module.exports = { timeToMinutes, calculateTokens, minutesToTime };
